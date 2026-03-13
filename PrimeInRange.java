3a694f8297ddba629de0ebde5fe0bb56e6363a38
Heins Devasia <heinsdevasia@gmail.com>
2026-03-13 21:38:02 +0530

    Java program to print prime numbers in a range using nested loops

---
+++ PrimeInRange.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class PrimeInRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the starting number: ");
+        int start = sc.nextInt();
+        System.out.print("Enter the ending number: ");
+        int end = sc.nextInt();
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+        for (int i = start; i <= end; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
+}
