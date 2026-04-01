57b66a065dddacc5b94c65e9c8dea6fd452aed19
Heins Devasia <heinsdevasia@gmail.com>
2026-04-01 20:52:37 +0530

    Anagram checker using string and array sorting

---
+++ AnagramChecker.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramChecker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine().toLowerCase();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine().toLowerCase();
+        str1 = str1.replaceAll("\\s", "");
+        str2 = str2.replaceAll("\\s", "");
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+
+        sc.close();
+    }
+}
