4e87b77d0aa5b4456664410e4166649c7d615922
Heins Devasia <heinsdevasia@gmail.com>
2026-04-03 21:49:03 +0530

    Enhance anagram checker with cleaner input processing

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -8,23 +8,26 @@ public class AnagramChecker {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter first string: ");
-        String str1 = sc.nextLine().toLowerCase();
+        String str1 = sc.nextLine().toLowerCase().replaceAll("\\s+", "");
 
         System.out.print("Enter second string: ");
-        String str2 = sc.nextLine().toLowerCase();
-        str1 = str1.replaceAll("\\s", "");
-        str2 = str2.replaceAll("\\s", "");
+        String str2 = sc.nextLine().toLowerCase().replaceAll("\\s+", "");
+
+        if (str1.length() != str2.length()) {
+            System.out.println("The strings are not anagrams.");
+            sc.close();
+            return;
+        }
+
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
 
         Arrays.sort(arr1);
         Arrays.sort(arr2);
 
-        if (Arrays.equals(arr1, arr2)) {
-            System.out.println("The strings are anagrams.");
-        } else {
-            System.out.println("The strings are not anagrams.");
-        }
+        System.out.println(Arrays.equals(arr1, arr2) 
+                ? "The strings are anagrams." 
+                : "The strings are not anagrams.");
 
         sc.close();
     }
