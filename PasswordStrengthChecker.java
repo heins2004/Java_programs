dc7e681e4e12b32bc934376bd8fa5ad8b239e949
Heins Devasia <heinsdevasia@gmail.com>
2026-04-04 21:19:32 +0530

    Improve password checker with strength scoring and validation

--- PasswordStrengthChecker.java
+++ PasswordStrengthChecker.java
@@ -1,29 +1,62 @@
 import java.util.Scanner;
+
 public class PasswordStrengthChecker {
+
+    public static int calculateStrength(String password) {
+        int score = 0;
+
+        if (password.length() >= 8) score++;
+        if (password.length() >= 12) score++;
+
+        if (password.matches(".*[A-Z].*")) score++;
+        if (password.matches(".*[a-z].*")) score++; 
+        if (password.matches(".*\\d.*")) score++;
+        if (password.matches(".*[@#$%^&+=!].*")) score++;
+
+        return score;
+    }
+
+    public static String getStrengthLevel(int score) {
+        if (score <= 2) return "Very Weak";
+        else if (score == 3) return "Weak";
+        else if (score == 4) return "Moderate";
+        else if (score == 5) return "Strong";
+        else return "Very Strong";
+    }
+
+    public static void suggestImprovements(String password) {
+        if (password.length() < 8)
+            System.out.println("- Use at least 8 characters");
+
+        if (!password.matches(".*[A-Z].*"))
+            System.out.println("- Add uppercase letters");
+
+        if (!password.matches(".*[a-z].*"))
+            System.out.println("- Add lowercase letters");
+
+        if (!password.matches(".*\\d.*"))
+            System.out.println("- Include numbers");
+
+        if (!password.matches(".*[@#$%^&+=!].*"))
+            System.out.println("- Add special characters (@#$%^&+=!)");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter your password: ");
         String password = sc.nextLine();
-        boolean hasUpper = false;
-        boolean hasLower = false;
-        boolean hasDigit = false;
-        for (int i = 0; i < password.length(); i++) {
-            char ch = password.charAt(i);
-            if (Character.isUpperCase(ch)) {
-                hasUpper = true;
-            } 
-            else if (Character.isLowerCase(ch)) {
-                hasLower = true;
-            } 
-            else if (Character.isDigit(ch)) {
-                hasDigit = true;
-            }
+
+        int score = calculateStrength(password);
+        String strength = getStrengthLevel(score);
+
+        System.out.println("Password Strength: " + strength);
+
+        if (score < 5) {
+            System.out.println("Suggestions to improve:");
+            suggestImprovements(password);
         }
-        if (password.length() >= 8 && hasUpper && hasLower && hasDigit) {
-            System.out.println("Strong Password");
-        } else {
-            System.out.println("Weak Password");
-        }
+
         sc.close();
     }
 }
